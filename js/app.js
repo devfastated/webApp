9adbbd48522d24625dd5f965822d29cf94a36350
Romer Jasen Jimenez <businessmailromer@gmail.com>
2023-10-20 22:06:13 +0800

    New Commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const person = [
+    {
+        name: "Garry Owen",
+        age: 21,
+        car: "Lambo"
+    }
+];
+
+document.getElementById("name").innerHTML = `
+    
+    <h1> He is ${person.name}</h1>
+`
